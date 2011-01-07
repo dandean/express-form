e5c1d068ab08d15f30d871063d0d8a5127031ba1
dandean <danwdean@gmail.com>
2011-01-07 00:33:34 -0800

    Initial reworking of the filter code. Fucking awesome.

--- test/main.test.js
+++ test/main.test.js
@@ -1,71 +1,36 @@
 var assert = require("assert"),
-    validation = require("../index"),
-    f = validation.filter,
-    v = validation.validator,
-    validationMiddleware = validation({
-      "age": {
-        filter: ["trim", "toInt"],
-        validate: "isInt"
-      },
-      "birthyear": {
-        filter: "toInt",
-        validate: "required -> birthyear is a required field"
-      },
-      "graduationyear": {
-        filter: "toInt",
-        validate: ["required", "isInt", "len(4,4)", "isLowercase"]
-      },
-      "username": {
-        filter: [
-          "ltrim('d ')",
-          function(value) { return value.toUpperCase(); }
-        ]
-      }
-//    }),
-//    validationMiddleware2 = validation({
-//      "age": {
-//        filter: f.trim().toInt(),
-//        validate: v.isInt("Age must be an integer")
-//      },
-//      "birthyear": {
-//        filter: f.toInt(),
-//        validate: v.required("birthyear is a required field")
-//      },
-//      "graduationyear": {
-//        filter: f.toInt(),
-//        validate: v.required("%s is a required field")
-//                   .isInt("%s must be in integer")
-//                   .len(4,4, "%s must be four digits long")
-//                   .isLowercase("%s must use lowercase letters")
-//      },
-//      "username": {
-//        filter: f.ltrim('d ')
-//                 .custom(function(value) {
-//                   return value.toUpperCase();
-//                 })
-//      }
-    })
-    ;
+    form = require("../index"),
+    filter = form.filter,
+    validationMiddleware = form(
+      filter("title").entityEncode(),
+      filter("age").entityDecode().trim().toInt(),
+      filter("birthyear").toInt(),
+      filter("graduationyear").toInt(),
+      filter("username").ltrim("d "),
+      filter("exclamations").custom(function() { return "!!!"; })
+    );
 
 module.exports = {
 
-  '0. ping server': function() {
+  '0. basic': function() {
     var request = {
       body: {
+        exclamations: "NOT EXCLAMATIONS",
+        title: "<h1>TITLE</h1>",
         age: "   5000  ",
-//        birthyear: "1978",
+        birthyear: "1978",
         graduationyear: "2003",
         username: "  dandean"
       }
     };
     
-    var response = {};
+    validationMiddleware(request, {});
     
-    validationMiddleware(request, response);
+    console.log(request);
     
     assert.ok(typeof request.body.age == 'number', "Should have converted `age` to an Int");
     assert.equal(5000, request.body.age);
-    assert.equal('ANDEAN', request.body.username);
+    assert.equal('andean', request.body.username);
   },
   
   '1. configuration': function() {
